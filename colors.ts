1e05d92dd12e3b623159fbfb2f39d4ba5abc04b1
Mormputchay <128577435+Mormputchay@users.noreply.github.com>
2023-03-22 14:54:31 +0700

    Add files via upload

---
+++ colors.ts
@@ -0,0 +1,28 @@
+const COLORS = {
+  Black: '#000',
+  Smoke: '#888',
+  white: '#f6f6f6',
+  BG_Color: '#0089',
+  gray: '#525252',
+  red: '#F52A2A',
+  light: '#E9E9E9',
+  green: '#00B761',
+  LIGHT_GREEN: '#CEE8E7',
+  LIGHT_GREY: '#F8F7F7',
+  LIGHT_GREY2: '#EAEAEA',
+  LIGHT_YELLOW: '#FCE6CD',
+  LIGHT_RED: '#FFC8BF',
+  FABEBOOK_BLUE: '#4A61A8',
+  GOOGLE_BLUE: '#53A0F4',
+  INACTIVE_GREY: '#A3A3A3',
+  DARK_ONE: '#121212',
+  DARK_TWO: '#181818',
+  DARK_THREE: '#404040',
+  DARK_FOUR: '#282828',
+  DARK_FIVE: '#B3B3B3',
+  Yellow: '#ffbf00',
+  Brown: '#5C4033',
+  Skyblue: '#005D86'
+};
+
+export default COLORS;
